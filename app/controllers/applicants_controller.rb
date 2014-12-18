c4e77c0dbf5a6c597af9f07a18096ef1966fddff
Dan Thomas <dan.thomas@dc.gov>
2014-12-18 08:19:41 -0500

    Initial commit

---
+++ app/controllers/applicants_controller.rb
@@ -0,0 +1,19 @@
+class ApplicantsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def link_employee
+  end
+
+  def challenge_identity
+  end
+end
